19097024196e2837a19c894ffa3f72e359ee286b
AmetDev <ametsejdaliev@gmail.com>
2024-03-31 22:32:10 +0300

    5-done

---
+++ Library/global.cpp
@@ -0,0 +1,7 @@
+// global.cpp
+
+#include "global.h"
+
+QString Global::getDatabasePath() {
+    return "/run/media/amexdev/tom/qt_4/qt_creator_edu_repos/untitled/db/library.db";
+}
--- untitled/global.cpp
+++
@@ -1,7 +0,0 @@
-// global.cpp
-
-#include "global.h"
-
-QString Global::getDatabasePath() {
-    return "/home/amex/Документы/repos/qt_creator_edu_repos/untitled/db/library.db";
-}
